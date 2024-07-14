814a2e7c6185eb26354492e345058ff1a8901e71
ayush gautam <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-14 20:46:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
+while True:
