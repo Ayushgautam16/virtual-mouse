af83732ec85898743d9a702393d902f2dbc90cdb
ayush gautam <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-14 21:08:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
 while True:
-   _, frame = cap.read()
+    _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -24,3 +24,16 @@ while True:
                     cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
                     index_x = screen_width/frame_width*x
                     index_y = screen_height/frame_height*y
+
+                if id == 4:
+                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+                    thumb_x = screen_width/frame_width*x
+                    thumb_y = screen_height/frame_height*y
+                    print('outside', abs(index_y - thumb_y))
+                    if abs(index_y - thumb_y) < 20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
+                    elif abs(index_y - thumb_y) < 100:
+                        pyautogui.moveTo(index_x, index_y)
+    cv2.imshow('Virtual Mouse', frame)
+    cv2.waitKey(1)
