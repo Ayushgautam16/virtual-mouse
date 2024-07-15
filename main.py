c4ab0d359d6b2002cb0913d1f7a7ea4f68ac8d43
ayush gautam <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-15 21:58:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
+# while loop
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
